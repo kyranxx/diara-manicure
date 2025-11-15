7919dd44cb63af044ccb5590cc938c062ae9ef9a
kyranxx <blanarikdaniel@gmail.com>
2025-11-15 18:13:24 +0100

    Revert "Correct Slovak testimonial text"
    This reverts commit ae9e23c0a23cae8faf37eb370f5fbf7df9439c25.

--- app/page.tsx
+++ app/page.tsx
@@ -312,7 +312,7 @@ export default function Home() {
                 author: "Katka R."
               },
               {
-                text: "Som veľmi spokojná, nechty mi vydržali celé t",
+                text: "Som veľmi spokojná, nechty sú mi vydržali celé týždne do dalšieho termínu. Vidno, že použiva pani manikérka kvalitný materiál. Spokojnosť.",
                 author: "Peťa S."
               }
             ].map((testimonial, index) => (
