df6cc5ef3eb1722efc429dd6ab2fe00b301d73c4
kyranxx <blanarikdaniel@gmail.com>
2025-11-26 16:20:18 +0100

    fix: resolve typescript error in facebook chat component

--- components/facebook-chat.tsx
+++ components/facebook-chat.tsx
@@ -1,26 +1,43 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import Script from 'next/script'
 
 export function FacebookChat() {
+  const chatboxRef = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
-    const chatbox = document.getElementById('fb-customer-chat')
-    if (chatbox) {
-      chatbox.setAttribute("page_id", "791288010744786")
-      chatbox.setAttribute("attribution", "biz_inbox")
+    // Ensure attributes are set
+    if (chatboxRef.current) {
+      chatboxRef.current.setAttribute("page_id", "791288010744786")
+      chatboxRef.current.setAttribute("attribution", "biz_inbox")
+    }
+
+    // If FB SDK is already loaded, re-parse
+    if (window.FB) {
+      window.FB.XFBML.parse()
     }
   }, [])
 
   return (
     <>
       <div id="fb-root" />
-      <div id="fb-customer-chat" className="fb-customerchat" />
+      <div 
+        id="fb-customer-chat" 
+        className="fb-customerchat"
+        ref={chatboxRef}
+      />
       <Script
         id="messenger-tag"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            if (chatbox) {
+              chatbox.setAttribute("page_id", "791288010744786");
+              chatbox.setAttribute("attribution", "biz_inbox");
+            }
+
             window.fbAsyncInit = function() {
               FB.init({
                 xfbml            : true,
