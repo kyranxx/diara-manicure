08bb08c9d535f53123aecbb03b4d8e1a29ca7082
kyranxx <blanarikdaniel@gmail.com>
2025-12-04 17:50:24 +0100

    Add Microsoft Clarity analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,15 @@ export default function RootLayout({
             gtag('config', 'AW-17746151386');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ugccqd16dq");
+          `}
+        </Script>
       </head>
       <body className={`${dmSans.className} ${dmSans.variable}`}>
         <ThemeProvider
