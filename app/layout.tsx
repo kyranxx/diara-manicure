f18cd6d8d45473b6bfad7302041ad80ecfc4366e
kyranxx <blanarikdaniel@gmail.com>
2025-12-09 00:58:53 +0100

    Add Google Analytics 4 tracking (G-QCMMZCQZTP)

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
           strategy="beforeInteractive"
         />
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17746151386"
+          src="https://www.googletagmanager.com/gtag/js?id=G-QCMMZCQZTP"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -64,6 +64,7 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
+            gtag('config', 'G-QCMMZCQZTP');
             gtag('config', 'AW-17746151386');
           `}
         </Script>
