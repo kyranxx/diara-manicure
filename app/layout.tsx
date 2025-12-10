937f917eedda354106d0ec7d05b721dec24ab341
kyranxx <blanarikdaniel@gmail.com>
2025-12-10 14:59:26 +0100

    Remove navbar FB chat button, fix Messenger link, toggle-only dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,8 +81,7 @@ export default function RootLayout({
       <body className={`${dmSans.className} ${dmSans.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           {children}
